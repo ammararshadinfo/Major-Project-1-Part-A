03562cf2710f6b12f3c9bfbfabae7f477447fcc9
Ammar Arshad <arshadammar1@gmail.com>
2024-03-13 21:36:08 +0530

    Your descriptive commit message here

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
-
+const methodOverride=require("method-override");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const ejsMate=require("ejs-mate");
+
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+app.use(express.json());
 main().then(()=>{
     console.log("Connected to DB");
 }).catch((err)=>{
@@ -17,6 +20,9 @@ main().then(()=>{
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
     res.send("Hi I am Root");
@@ -28,10 +34,19 @@ app.get("/listings", async(req,res)=>{
     res.render("listings/index.ejs",{allListings});
 });
 
+//Create Route
+
+app.post("/listings", async (req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+console.log(newListing);
+    res.redirect("/listings");
+});
+
 //New Route
 
 app.get("/listings/new",(req, res)=>{
-    res.render("/listings/new.ejs");
+    res.render("listings/new.ejs");
 });
 
 //SHow Route
@@ -40,9 +55,32 @@ app.get("/listings/:id",async (req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+    console.log(listing);
+});
+//edit route
+
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
 });
 
+//Update Route
 
+app.put("/listings/:id", async (req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+
+app.delete("/listings/:id", async (req, res)=>{
+    let {id}=req.params;
+    let deletedListing= await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing=new Listing({
