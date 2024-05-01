2df5ce0d695e9d4c86819a81d9d63da3d477bac8
Ammar Arshad <arshadammar1@gmail.com>
2024-05-01 17:10:18 +0530

    Your descriptive commit message here

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,7 @@
 const Listing = require("../models/listing");
-
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
@@ -26,12 +28,21 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-  await newListing.save();
+  newListing.geometry = response.body.features[0].geometry;
+  let savedListing = await newListing.save();
+  console.log(savedListing);
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
@@ -44,7 +55,10 @@ module.exports.renderEditForm = async (req, res) => {
     res.redirect("/listings");
   }
   let originalImageUrl = listing.image.url;
-  originalImageUrl = originalImageUrl.replace("/uploads", "/uploads/h_50,w_50");
+  originalImageUrl = originalImageUrl.replace(
+    "/uploads",
+    "/uploads/c_fill,h_200,w_350"
+  );
   res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
